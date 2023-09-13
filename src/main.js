753a5259f333f8ceeb32c1a312c78f58934dfce1
Adrian Melgar <DaikiMT@outlook.es>
2023-09-13 14:13:16 -0500

    Added primevue component library and theme

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
 
-createApp(App).mount('#app')
+// App Theme
+import 'primevue/resources/themes/mdc-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+// PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add PrimeVue Components
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app')
