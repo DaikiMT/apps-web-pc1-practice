55d2fb071c37ff1f489ad835837c5f79c334f1e8
Adrian Melgar <DaikiMT@outlook.es>
2023-09-13 16:46:06 -0500

    Addedcomponent for handling of unavailable new

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h4>{{ $t('unavailable-news')}}</h4>
+    <div v-for="error in errors">
+      <h6>{{ error }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "unavailable-content",
+  props: {
+    errors: null,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
